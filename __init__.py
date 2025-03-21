1b0482d9585dc847391affba03b2c598c3f82ab9
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-03-21 11:53:33 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
+from .OrpheusTTS_nodes import SingleTextGeneration, LongTextGeneration
 
+NODE_CLASS_MAPPINGS = {
+    "Single Text Generation": SingleTextGeneration,
+    "Long Text Generation": LongTextGeneration
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SingleTextGeneration": "Single Text Generation",
+    "LongTextGeneration": "Long Text Generation"
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
